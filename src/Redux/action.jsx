2eb2b0ea1d11bf1a67f6659a2362b8ca02389df8
DantePc <djkmecdgm65@gmail.com>
2023-08-29 01:46:07 -0500

    agregando url peployado

--- src/Redux/action.jsx
+++ src/Redux/action.jsx
@@ -3,10 +3,15 @@ export const POST_URL = "POST_URL"
 export const GET_URL = "GET_URL"
 export const OCULT_HISTORY = "OCULT_HISTORY"
 
+const urlGetLocal = "http://localhost:3001/getUrl"
+const urlGet = "https://short-links-bh20.onrender.com/getUrl"
+const urlPostLocal = "http://localhost:3001/url"
+const urlPost = "https://short-links-bh20.onrender.com/url"
+
 export const getUrls = () => {
     try {
         return async function (dispatch) {
-            const urls = await axios.get("http://localhost:3001/getUrl")
+            const urls = await axios.get(urlGet)
             localStorage.setItem("result", JSON.stringify(urls.data)); // actualizo el local inmediatamente
             return dispatch({
                 type : GET_URL,
@@ -21,7 +26,7 @@ export const getUrls = () => {
 export const postUrl = (dato) => {
     try {
         return async function (dispatch) {
-            const url = await axios.post("http://localhost:3001/url", dato)
+            const url = await axios.post(urlPost, dato)
             return dispatch({
                 type : POST_URL,
                 payload : url.data
